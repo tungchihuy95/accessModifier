fdb454951748172432da8ddb1831811ae8bc6565
tungchihuy95 <tungjpvn95@gmail.com>
2020-07-09 16:18:45 +0700

    Demo

--- src/Demo/TestNV.java
+++ src/Demo/TestNV.java
@@ -1,4 +1,10 @@
 package Demo;
 
 public class TestNV {
+    public static void main(String[] args) {
+        Nhanvien nhanvien = new Nhanvien(5,"Tung", "Giam doc");
+        System.out.println("ID:" + nhanvien.getId() + "-" + "Ten:" + nhanvien.getTen() + "-" + "Phong:" + nhanvien.getPhong());
+        nhanvien.setId(3);
+        System.out.println("ID:" + nhanvien.getId() + "-" + "Ten:" + nhanvien.getTen() + "-" + "Phong:" + nhanvien.getPhong());
+    }
 }
